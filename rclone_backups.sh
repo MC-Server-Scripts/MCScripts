7997d6ffbe5cbdb147c259e55c6526846788bf0b
IcaroK <icarokaueroque@gmail.com>
2023-12-23 13:02:29 -0300

    Added basic zip functionality. It will zip the file and add the comment.

--- rclone_backups.sh
+++ rclone_backups.sh
@@ -1,5 +1,5 @@
 #
-#
+# MADE WITH ASSISTANCE OF CHATGPT AND STACK OVERFLOW
 #
 # FEATURES
 #
@@ -15,8 +15,8 @@
 #
 # To do this, it will follow this order:
 #
-# 1) Be called when the minecraft server .sh script is run
-# 2) Wait until the server script is finished running, i.e the world is saved, all the players have left and server is shut down.
+# 1) Be called when the minecraft server .sh script is run                                                                                           SEE STARTSERVER-JAVA9.SH
+# 2) Wait until the server script is finished running, i.e the world is saved, all the players have left and server is shut down.                    SEE STARTSERVER-JAVA9.SH
 # 3) Upon confirmation that the script is not running, it will use zip to create a zip file to the following specifications:
 #     3a) It will include, recursively, the entire "World" folder.
 #     3b) The file's name will be determined by: MODPACKNAME_DATE_SIZEOFWORLDFOLDER.zip
@@ -37,5 +37,26 @@
 # 7*) After every backup, it will send the email log to the user
 #
 # NOTE: THE * order are optional and low-priority for development.
+
+# 1) Start Modpack HUB Script is called -> Pick server wanted -> Calls startserver-modpack.sh script -> In game, /stop command is sent (op 4 needed) -> startserver-modpack.sh stops running -> calls rclone_backup_modpack.sh
+
+
+# GET VARIABLES
+modpack_name=$(basename "$(pwd)")
+current_date=$(date '+%Y-%m-%d')
+world_size=$(du -hs World/ | cut -f1)
+zip_name="${modpack_name}_${world_size}_${current_date}"
+precise_date=$(date '+%H:%M:%S.%3N')
+time_zone=$(date '+%:z')
+easy_time_log="[UTC${time_zone}][${precise_date}]:"
+
+
+
+# CREATE COMMENT AND ZIP FILE
+zip_comment="This zipfile was created at ${current_date}, is part of the ${modpack_name} modpack and has ${world_size}Bs. It has been shipped to Dropbox, GDrive and MEGA. In case Oracle ever deletes this server instance, this is a way to have the world files."
+echo "${zip_comment}" > zip_comment_file.txt
+zip -r -9 "${zip_name}" World/
+zip -z "${zip_name}".zip < zip_comment_file.txt
+rm zip_comment_file.txt
 
 
