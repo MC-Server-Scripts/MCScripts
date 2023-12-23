8e2a105069d8b4a0c92d7dd0199a162408695cbd
IcaroK <icarokaueroque@gmail.com>
2023-12-23 15:46:07 -0300

    Finished the Main script. Will update it later when I need to add more modpacks OR email functionality.

--- rclone_backups.sh
+++ rclone_backups.sh
@@ -1,5 +1,5 @@
 #
-# MADE WITH ASSISTANCE OF CHATGPT AND STACK OVERFLOW
+# MADE WITH ASSISTANCE OF CHATGPT, STACK OVERFLOW AND THE RCLONE FORUMS
 #
 # FEATURES
 #
@@ -28,9 +28,9 @@
 #     4a) GDrive
 #     4b) Dropbox
 #     4c) Mega
-# 5) After confirmation of all three files being sent, it will delete the zip file and wait for new backups.
-#     5a) NOTE: IF THE FILE TAKES OVER 15 MINUTES TO SEND, IT WILL SKIP THE PROVIDER. IF ALL PROVIDERS FAIL, IT WILL SEND AN URGENT EMAIL NOTIFICATION TO THE USER TO ALERT THEM.
-#     5b) IF ORACLE EVER STARTS THREATENING THE INTEGRITY OF THE SERVER, A FUNCTION CALLED "PANIC MODE" WILL BE RUN THAT WILL SEND AN EMAIL NOTIFICATION TO THE USER, LOGGING THE DATE OF THE INCIDENT AND WILL BACKUP EVERY WORLD AND SEND THEM TO THE PROVIDERS, REGARDLESS OF THE DATE OF THE LAST BACKUP
+# 5*) After confirmation of all three files being sent, it will delete the zip file and wait for new backups.
+#     5a*) NOTE: IF THE FILE TAKES OVER 15 MINUTES TO SEND, IT WILL SKIP THE PROVIDER. IF ALL PROVIDERS FAIL, IT WILL SEND AN URGENT EMAIL NOTIFICATION TO THE USER TO ALERT THEM.
+#     5b*) IF ORACLE EVER STARTS THREATENING THE INTEGRITY OF THE SERVER, A FUNCTION CALLED "PANIC MODE" WILL BE RUN THAT WILL SEND AN EMAIL NOTIFICATION TO THE USER, LOGGING THE DATE OF THE INCIDENT AND WILL BACKUP EVERY WORLD AND SEND THEM TO THE PROVIDERS, REGARDLESS OF THE DATE OF THE LAST BACKUP
 # 6*) It should log every step to a file named log.txt
 #     6*a) It should be formatted like this:
 #         [timestamp with year-month-day, hour:minute:second] STEP OF THE THINGY
@@ -41,22 +41,101 @@
 # 1) Start Modpack HUB Script is called -> Pick server wanted -> Calls startserver-modpack.sh script -> In game, /stop command is sent (op 4 needed) -> startserver-modpack.sh stops running -> calls rclone_backup_modpack.sh
 
 
-# GET VARIABLES
+### CONFIGURATION ZONE ###
+
+# CONFIGURE HOW MANY BACKUPS SHOULD BE KEPT FOR EACH MODPACK, IN EACH PROVIDER
+
+gdrive_cutoff_number=3
+mega_cutoff_number=4
+dropbox_cutoff_number=1
+
+
+
+
+### CONFIGURATION ZONE ###
+
+
+
+
+
+# GET VARIABLES 
 modpack_name=$(basename "$(pwd)")
 current_date=$(date '+%Y-%m-%d')
 world_size=$(du -hs World/ | cut -f1)
-zip_name="${modpack_name}_${world_size}_${current_date}"
+zip_name="${modpack_name}_${current_date}_${world_size}"
 precise_date=$(date '+%H:%M:%S.%3N')
 time_zone=$(date '+%:z')
-easy_time_log="[UTC${time_zone}][${precise_date}]:"
+easy_time_log="[UTC${time_zone}][${current_date}, ${precise_date}]:"
+logf="${modpack_name}_${current_date} log"
 
+#### PART 1 ####
 
-
-# CREATE COMMENT AND ZIP FILE
+# CREATE COMMENT, LOG AND ZIP FILE. LOG.
+echo "Creating ZIP File."
+touch "${logf}.txt"
 zip_comment="This zipfile was created at ${current_date}, is part of the ${modpack_name} modpack and has ${world_size}Bs. It has been shipped to Dropbox, GDrive and MEGA. In case Oracle ever deletes this server instance, this is a way to have the world files."
 echo "${zip_comment}" > zip_comment_file.txt
-zip -r -9 "${zip_name}" World/
+zip -q -r -9 "${zip_name}" World/
 zip -z "${zip_name}".zip < zip_comment_file.txt
 rm zip_comment_file.txt
 
+echo "Created zip file. Sending to providers."
+echo "${easy_time_log} Created zip world file. Size is "${world_size}"Bs." >> "${logf}.txt"
 
+
+#### PART 2 ####
+
+#SEND ZIP FILE TO REMOTES
+
+rclone sync --metadata "${zip_name}.zip" GDrive:backups_mc/"${modpack_name}"
+echo "${easy_time_log} Sent zip file to Google Drive." >> "${logf}.txt"
+rclone sync --metadata "${zip_name}.zip" Dropbox:backups_mc/"${modpack_name}"
+echo "${easy_time_log} Sent zip file to Dropbox." >> "${logf}.txt"
+rclone sync --metadata "${zip_name}.zip" MEGA:backups_mc/"${modpack_name}"
+echo "${easy_time_log} Sent zip file to MEGA." >> "${logf}.txt"
+
+echo "File has been sent to providers!"
+
+#REMOVE n-LATEST BACKUPS
+
+echo "Removing n-latest backups."
+
+gdrive_backups_delete="$(rclone lsf GDrive:backups_mc/"${modpack_name}" | sort | head -n -"${gdrive_cutoff_number}")"
+dropbox_backups_delete="$(rclone lsf Dropbox:backups_mc/"${modpack_name}" | sort | head -n -"${dropbox_cutoff_number}")"
+mega_backups_delete="$(rclone lsf MEGA:backups_mc/"${modpack_name}" | sort | head -n -"${mega_cutoff_number}")"
+
+echo "${gdrive_backups_delete}" > gdrive_backups.txt
+echo "${dropbox_backups_delete}" > dropbox_backups.txt
+echo "${mega_backups_delete}" > mega_backups.txt
+
+rclone delete GDrive:backups_mc/"${modpack_name}" --files-from "gdrive_backups.txt"
+echo "${easy_time_log} Deleted latest GDrive backups." >> "${logf}.txt"
+rclone delete Dropbox:backups_mc/"${modpack_name}" --files-from "dropbox_backups.txt"
+echo "${easy_time_log} Deleted latest Dropbox backups." >> "${logf}.txt"
+rclone delete MEGA:backups_mc/"${modpack_name}" --files-from "mega_backups.txt"
+echo "${easy_time_log} Deleted latest MEGA backups." >> "${logf}.txt"
+
+echo "N-latest files removed!"
+
+rm gdrive_backups.txt
+rm mega_backups.txt
+rm dropbox_backups.txt
+
+#DELETE REMAINING ZIP FILE
+
+rm "${zip_name}.zip" 
+echo "${easy_time_log} ZIP FILE REMOVED!" >> "${logf}.txt"
+
+echo "Remaining zip file deleted!"
+
+#insert mail functionality here
+
+echo "${easy_time_log} Sending myself to the backup providers." >> "${logf}.txt"
+echo "${easy_time_log} Tasks over." >> "${logf}.txt"
+
+rclone sync --metadata "${logf}.txt" GDrive:backups_mc/"${modpack_name}"
+rclone sync --metadata "${logf}.txt" Dropbox:backups_mc/"${modpack_name}"
+rclone sync --metadata "${logf}.txt" MEGA:backups_mc/"${modpack_name}"
+
+echo "I am finished here!"
+exit 
